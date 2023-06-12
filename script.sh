e49f0c7f2af8ddfd1a4a0f3b589678e2f94ccc31
Basserti <51204419+Basserti@users.noreply.github.com>
2023-06-12 23:39:36 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 cp runs/detect/train1/weights/best.pt models/best.pt
 rm -R runs
+dvc add models/best.pt
+dvc commit
+dvc push
+git add models/best.pt.dvc
+git commit -m 'dataset2'
+gut push origins docker_train
