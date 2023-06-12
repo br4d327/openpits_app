705b5ecd7fd1cb8cde03485355bba89c761b4cbe
Basserti <51204419+Basserti@users.noreply.github.com>
2023-06-12 23:22:06 +0500

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO("models/yolov8m.pt")
-model.train(data='dataset.yaml', imgsz=518, batch=16, task='detect', device=0, epochs=20)
+model.train(data='dataset.yaml', imgsz=518, batch=8, task='detect', device=0, epochs=20)
