fd0f7975128a120a9f42e8c92ba3a6dbc0c3847f
Basserti <51204419+Basserti@users.noreply.github.com>
2023-06-14 10:51:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def image_input(mult_files):
     if mult_files and image_file is not None:
         for img_f in image_file:
             ts = datetime.timestamp(datetime.now())
-            img_path = os.path.join(r'data/uploads', str(ts) + '_' + img_f.name)
+            img_path = os.path.join(r'data/uploads', str(ts) + '#' + img_f.name)
 
             with open(img_path, mode="wb") as f:
                 f.write(img_f.getbuffer())
@@ -46,7 +46,7 @@ def image_input(mult_files):
 
     elif image_file is not None:
             ts = datetime.timestamp(datetime.now())
-            img_path = os.path.join(r'data/uploads', str(ts) + '_' + image_file.name)
+            img_path = os.path.join(r'data/uploads', str(ts) + '#' + image_file.name)
 
             with open(img_path, mode="wb") as f:
                 f.write(image_file.getbuffer())
@@ -135,10 +135,10 @@ if __name__ == '__main__':
         for file in os.listdir(label_latest_dir):
             print(file)
             st.text(file)
-            if file.split('/')[-1][:-4] in current_uploaded_img:
-                print(label_latest_dir + file)
-                table = img_summary(label_latest_dir + file)
-                res_table = pd.concat([res_table, table])
+            
+            print(label_latest_dir + file)
+            table = img_summary(label_latest_dir + file)
+            res_table = pd.concat([res_table, table])
 
         res_table.reset_index(drop=True)
         st.table(res_table)
